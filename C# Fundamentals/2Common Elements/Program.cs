3ca7d63e32babd00f4437add7c4888a80eda0981
Petar <pepoli.2000@abv.bg>
2022-06-05 18:14:08 +0300

    Added more exercises

---
+++ C# Fundamentals/2Common Elements/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Common_Elements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input1 = Console.ReadLine();
+            string input2 = Console.ReadLine();
+            string[] arr1 = input1.Split(" ");
+            string[] arr2 = input2.Split(" ");
+            for (int i = 0; i < arr2.Length; i++)
+                for (int j = 0; j < arr1.Length; j++)
+                {
+                    if (arr2[i] == arr1[j])
+                    {
+                        Console.Write(arr1[j] + " ");
+                    }
+                }
+        }
+    }
+}
